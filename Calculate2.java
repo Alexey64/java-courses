2ae3b004daf763f6112d82ce8ab3e10ad1d82601
Alexey64 <alexey64.ru@gmail.com>
2016-12-15 16:37:09 +0400

    New Calculate

--- Calculate2.java
+++ Calculate2.java
@@ -2,6 +2,9 @@ public class Calculate2 {
 	
 	public static void main(String[] arg) {
 		System.out.println("Loading....");
-		
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int summ = first + second;
+		System.out.println("Summ " + summ);
 	}
 }
---
+++ Calculate3.java
@@ -0,0 +1,5 @@
+public class Calculate3 {
+	public static void main(String[] arg) {
+System.out.println("New Calculate!");	
+	}
+}
